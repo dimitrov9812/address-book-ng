7e561db95803f0359ef030bed794aa34a77e619f
Alexander Dimitrow <alexanderdimitrov333@gmail.com>
2021-01-04 09:35:05 +0200

    Initial commit, created the main component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { AddressListComponent } from './address-list/address-list.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressFormComponent,
+    AddressListComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
